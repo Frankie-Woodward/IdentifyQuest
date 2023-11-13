9320bc59f8ccacb50fd513e70e12cdca409a3d7a
Frankie <fwoodwar@gmail.com>
2023-11-13 12:28:43 -0700

    updated vite config and .env moved to root

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    root: './identify-quest/frontend',
+    root: path.resolve(__dirname, 'identify-quest/frontend'),
     // envFile: path.resolve(__dirname, '.env'),
     server: {
         proxy: {
